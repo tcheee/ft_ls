c81149f84c3e78ab07fab5162b1ed57547e4de58
Thomas Cherret <tcherret@e1z4r12p3.42.us.org>
2019-02-20 15:35:23 -0800

    gestion link

--- srcs/display_list.c
+++ srcs/display_list.c
@@ -6,13 +6,25 @@
 /*   By: tcherret <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 11:39:52 by tcherret          #+#    #+#             */
-/*   Updated: 2019/02/19 22:26:40 by tcherret         ###   ########.fr       */
+/*   Updated: 2019/02/20 15:35:01 by tcherret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-//static int	checking_lnk()
+static int	checking_lnk(char *name, t_option *opt)
+{
+	struct stat	info_lnk;
+
+	if (lstat(name, &info_lnk) == -1)
+		return (-1);
+	if (opt->l == 1 && S_ISLNK(info_lnk.st_mode))
+	{
+		ft_inspect("", name, opt);
+		return (1);
+	}
+	return (0);
+}
 
 void		display_list(char **list, int j, t_option *opt, char *name)
 {
@@ -20,7 +32,7 @@ void		display_list(char **list, int j, t_option *opt, char *name)
 	char	*stock;
 
 	i = -1;
-	while (++i < j - 1)
+	while (++i < j)
 		if (list[i] == NULL)
 			return ;
 	stock = ft_strdup(name);
@@ -32,11 +44,15 @@ void		display_list(char **list, int j, t_option *opt, char *name)
 		list = ft_reverse_sort(list, opt->elem - 1);
 	else
 		list = ft_sort(list, opt->elem - 1);
-	//if (checking_lnk() == 1)
+	if (opt->l == 1 && checking_lnk(stock, opt) == 1)
+	{
+		free(stock);
+		return ;
+	}
 	i = 0;
 	if (opt->l >= 1)
 		ft_printf("total %d\n", opt->tot);
-	while (i < j - 1)
+	while (i < j)
 	{
 		if ((opt->a && list[i][0] == '.') || list[i][0] != '.')
 			opt->l == 0 ?
